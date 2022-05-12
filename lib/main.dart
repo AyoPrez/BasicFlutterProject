b011bb05900aed1c728f2551d9f649d993811450
Ayo <arezrod_16@hotmail.com>
2022-05-12 20:35:10 +0200

    Base code

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:lucha_fantasy/router/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    Routes.setupRouter();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppLocalizations.of(context).appName,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('es', ''),
+      ],
+      initialRoute: '/',
+      onGenerateRoute: Routes.router.generator,
+    );
+  }
+}
