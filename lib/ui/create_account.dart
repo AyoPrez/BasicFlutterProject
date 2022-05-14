618dd0ca760231fdbdf9f7c60ae773b31816d32c
Ayo <arezrod_16@hotmail.com>
2022-05-14 19:47:40 +0200

    Added hint to input text

--- lib/ui/create_account.dart
+++ lib/ui/create_account.dart
@@ -20,17 +20,42 @@ class _CreateAccountState extends State<CreateAccount> {
         body: Padding(
           padding: const EdgeInsets.all(80.0),
           child: Center(
-            child: Container(
+            child: SizedBox(
               width: 400,
               child: Column(
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: MouseRegion(child: TextField()),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: MouseRegion(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            icon: const Icon(Icons.mail),
+                            hintText: AppLocalizations.of(context).email),
+                      ),
+                    ),
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: MouseRegion(child: TextField()),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: MouseRegion(
+                      child: TextField(
+                        decoration: InputDecoration(
+                            icon: const Icon(Icons.account_box_outlined),
+                            hintText: AppLocalizations.of(context).username),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: MouseRegion(
+                      child: TextField(
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        decoration: InputDecoration(
+                            icon: const Icon(Icons.key),
+                            hintText: AppLocalizations.of(context).password),
+                      ),
+                    ),
                   ),
                   Wrap(children: [
                     Padding(
